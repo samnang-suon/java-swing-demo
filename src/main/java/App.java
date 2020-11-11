b52bb2a30f0c57d09d9154254ef32c05b86707ab
samnang-suon <samnang.suon.1@ens.etsmtl.ca>
2020-11-11 12:21:07 -0500

    Implement ActionListener

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,33 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class App extends JFrame {
+public class App extends JFrame implements ActionListener {
 
     public App() {
+        // Add controls/components
+        JPanel centerPanel = new JPanel();
+        JButton helloBtn = new JButton("Click Me");
+        helloBtn.setFont(new Font(Font.SANS_SERIF,  Font.BOLD, 24));
+        helloBtn.setSize(300, 200);
+        helloBtn.addActionListener(this);
+        centerPanel.add(helloBtn);
+        this.add(centerPanel, BorderLayout.CENTER);
+
         this.setTitle("Demo App");
         this.setSize(600, 400);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton temp = (JButton) e.getSource();
+        String name = JOptionPane.showInputDialog(null, "Please enter your name:", "Question", JOptionPane.QUESTION_MESSAGE);
+        temp.setText("Hi " + name);
+        temp.setFont(new Font(Font.SANS_SERIF,  Font.BOLD, 36));
     }
 
     public static void main(String[] args) {
